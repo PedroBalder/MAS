430466e561cd25c057e1653eec963f0423dcd297
AMaranhao <50458869+AMaranhao@users.noreply.github.com>
2023-10-05 00:13:45 -0300

    mudando transição pos denuncia

--- assets/js/script_transicao.js
+++ assets/js/script_transicao.js
@@ -3,7 +3,7 @@ const transitionTime = 3000; // 3 segundos
 
 // Função para redirecionar para a próxima tela após o tempo de transição
 function redirectToNextScreen() {
-    window.location.href = 'html_main.html'; // Substitua com a URL da próxima tela
+    window.location.href = 'index.html'; // Substitua com a URL da próxima tela
 }
 
 // Mostra a tela de transição por um período de tempo e, em seguida, redireciona
