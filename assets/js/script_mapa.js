bf85c54815d57df28347a90e425a0cde85e7b9e5
AMaranhao <alexandreqmj@gmail.com>
2023-10-08 15:20:50 -0300

    marcadores no mapa

--- assets/js/script_mapa.js
+++ assets/js/script_mapa.js
@@ -40,19 +40,70 @@ function handleLocationError(error) {
     }
 }
 
-// Verificar se o navegador suporta geolocalização
+
+
+// CARREGANDO DENUNCIAS NO MAPA
+
+
+function adicionarMarcadoresNoMapa(map, denuncias) {
+    denuncias.forEach(denuncia => {
+        const latitude = parseFloat(denuncia.latitude);
+        const longitude = parseFloat(denuncia.longitude);
+        const textoDenuncia = denuncia.textoDenuncia;
+
+        // Crie um objeto de ícone personalizado para este marcador
+        const customIcon = {
+            url: 'http://maps.google.com/mapfiles/ms/icons/green-dot.png', // URL do ícone personalizado
+            scaledSize: new google.maps.Size(30, 30), // Tamanho do ícone
+        };
+
+        // Crie um marcador para esta denúncia com o ícone personalizado
+        const marker = new google.maps.Marker({
+            position: { lat: latitude, lng: longitude },
+            map: map,
+            title: textoDenuncia,
+            icon: customIcon // Defina o ícone personalizado
+        });
+    });
+}
+
+// Carregar JSON de denúncias a partir do arquivo local
+function carregarDenunciasDoJSON(map) {
+    fetch('assets/json/denuncia.json')
+        .then(response => response.json())
+        .then(data => {
+            // Assim que os dados do JSON forem carregados, adicione os marcadores ao mapa
+            adicionarMarcadoresNoMapa(map, data.denuncia);
+        })
+        .catch(error => {
+            console.error('Erro ao carregar o arquivo JSON:', error);
+        });
+}
+
+// No momento em que você tem as coordenadas do mapa disponíveis
 if ('geolocation' in navigator) {
     navigator.geolocation.getCurrentPosition(function (position) {
         const latitude = parseFloat(position.coords.latitude);
         const longitude = parseFloat(position.coords.longitude);
-        initMap(latitude, longitude);
-        //console.log(`Latitude: ${latitude}`);
-        //console.log(`Longitude: ${longitude}`);
+        const mapOptions = {
+            center: { lat: latitude, lng: longitude },
+            zoom: 14
+        };
+        console.log(latitude);
+        console.log(longitude);
+        const map = new google.maps.Map(document.getElementById('map'), mapOptions);
+        
+        const userMarker = new google.maps.Marker({
+            position: { lat: latitude, lng: longitude },
+            map: map,
+            title: 'Sua Localização Atual'
+        });
+        // Carregue as denúncias do JSON e adicione marcadores ao mapa
+        carregarDenunciasDoJSON(map);
+
     }, function (error) {
         handleLocationError(error);
     });
 } else {
     alert('A geolocalização não é suportada neste navegador.');
 }
-
-
